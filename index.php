452019e6bc2cd12e951a1184c241cb400fc02701
Matthew Byrne <32844656+X17138744@users.noreply.github.com>
2019-09-28 00:09:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<!-- Codes by HTML.am -->
-
 <!-- CSS Code -->
 <style type="text/css" scoped>
 img.GeneratedImage {
@@ -8,4 +6,8 @@ width:700px;height:225px;margin:10px;border-width:6px;border-color:#000000;borde
 </style>
 
 <!-- HTML Code -->
-<img src="https://raw.githubusercontent.com/X17138744/Fetch-/master/FETCH!.png" alt="Photo of the Remarkables mountain range in Queenstown, New Zealand." class="GeneratedImage">
+<img src="https://raw.githubusercontent.com/X17138744/Fetch-/master/FETCH!.png" class="GeneratedImage">
+
+We are currently building!
+
+Come back soon!
