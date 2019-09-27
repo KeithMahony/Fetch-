8432b7c40f268d552207fd68aa67799541d5c954
Matthew Byrne <32844656+X17138744@users.noreply.github.com>
2019-09-27 23:01:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-Hello Fetch App Online Yo!
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+    <img src='path/to/FETCH!.png' alt='photo of me' />
+</body>
+</html>
