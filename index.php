9f3d049904ba20f736b9c3730affb31b73f26b63
Matthew Byrne <32844656+X17138744@users.noreply.github.com>
2019-10-03 15:48:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,14 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   
-  ![Screenshot](FETCH!.png)
+  
 </head>
 <body>
 
 <div class="jumbotron text-center">
   <h1>Fetch! Application</h1>
   <p>Bootstrap example. Come back soon!</p> 
+  <center>![Screenshot](FETCH!.png)</center>
 </div>
 </body>
 </html>
