67bd5654be1ad706c400d0c835d51aecef87e15b
Keith Mahony <36074197+KeithMahony@users.noreply.github.com>
2019-10-25 13:12:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,38 +2,17 @@
 	
  echo 'FetchApp Test';
 
-// Create connection
+// Createa a connection
 $con=mysqli_connect("us-cdbr-iron-east-05.cleardb.net","bfee5eec5d1d2a","49334008","heroku_030aad63729d0bd");
  
-// Check connection
+// Checks connection
 if (mysqli_connect_errno())
 {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
- 
-// Select all of our stocks from table 'stock_tracker'
-$sql = "SELECT * FROM stock_tracker";
- 
-// Confirm there are results
-if ($result = mysqli_query($con, $sql))
-{
- // We have results, create an array to hold the results
-        // and an array to hold the data
- $resultArray = array();
- $tempArray = array();
- 
- // Loop through each result
- while($row = $result->fetch_object())
- {
- // Add each result into the results array
- $tempArray = $row;
-     array_push($resultArray, $tempArray);
- }
- 
- // Encode the array to JSON and output the results
- echo json_encode($resultArray);
+else{
+  echo "Connected to MySQL";
 }
- 
-// Close connections
+// Close connection
 mysqli_close($con);
 ?>
