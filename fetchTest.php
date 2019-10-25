603ade6d5b03973af5333887a0f4dbf264fabca3
Keith Mahony <36074197+KeithMahony@users.noreply.github.com>
2019-10-25 10:21:55 +0100

    Update fetchTest.php

--- fetchTest.php
+++ fetchTest.php
@@ -1,11 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Fetch! Application Page</title>
-</head>
-<body>
 <?php
-	echo 'FetchApp Test';
+	
+ echo 'FetchApp Test';
+
+// Create connection
+$con=mysqli_connect("localhost","username","password","dbname");
+ 
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+ 
+// Select all of our stocks from table 'stock_tracker'
+$sql = "SELECT * FROM stock_tracker";
+ 
+// Confirm there are results
+if ($result = mysqli_query($con, $sql))
+{
+ // We have results, create an array to hold the results
+        // and an array to hold the data
+ $resultArray = array();
+ $tempArray = array();
+ 
+ // Loop through each result
+ while($row = $result->fetch_object())
+ {
+ // Add each result into the results array
+ $tempArray = $row;
+     array_push($resultArray, $tempArray);
+ }
+ 
+ // Encode the array to JSON and output the results
+ echo json_encode($resultArray);
+}
+ 
+// Close connections
+mysqli_close($con);
 ?>
-</body>
-</html>
