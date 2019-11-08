421373218ff5ad57b07fd94e1f11e7dcba24b62d
Keith Mahony <36074197+KeithMahony@users.noreply.github.com>
2019-11-08 09:16:19 +0000

    Create phpTest.php

---
+++ Android_connect/phpTest.php
@@ -0,0 +1,18 @@
+<?php
+	
+ echo 'FetchApp Test';
+// Createa a connection
+$con=mysqli_connect("us-cdbr-iron-east-05.cleardb.net","bfee5eec5d1d2a","49334008","heroku_030aad63729d0bd");
+ 
+// Checks connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+else{
+  echo "Connected to MySQL";
+}
+// Close connection
+mysqli_close($con);
+
+?>
