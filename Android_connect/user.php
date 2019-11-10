bfb2f3fbd57574f1045596918e425643e5008f10
Keith Mahony <36074197+KeithMahony@users.noreply.github.com>
2019-11-10 14:21:32 +0000

    Update user.php

--- Android_connect/user.php
+++ Android_connect/user.php
@@ -5,10 +5,10 @@
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
    }
 	
-   $username = $_POST['username'];
+   $email = $_POST['email'];
    $password = $_POST['password'];
-   $result = mysqli_query($con,"SELECT Role FROM table1 where 
-   Username='$username' and Password='$password'");
+   $result = mysqli_query($con,"SELECT Role FROM users where 
+   email='$email' and Password='$password'");
    $row = mysqli_fetch_array($result);
    $data = $row[0];
 
