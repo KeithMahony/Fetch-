800ca96ad0ab54a8c806d3b8ab89f4027721049e
Keith Mahony <36074197+KeithMahony@users.noreply.github.com>
2019-11-10 19:28:53 +0000

    Update index.php

--- Android_connect/index.php
+++ Android_connect/index.php
@@ -1,41 +1,35 @@
-<?php
-    
-    require_once 'user.php';
-    
-    $username = "";
-    
-    $password = "";
-    
-    $email = "";
-    
-    if(isset($_POST['username'])){
-        
-        $username = $_POST['username'];
-        
-    }
-    
-    if(isset($_POST['password'])){
-        
-        $password = $_POST['password'];
-        
-    }
-    
-    if(isset($_POST['email'])){
-        
-        $email = $_POST['email'];
-        
-    }
-    
-    $userObject = new User();
-    
-    // Login
-    
-    if(!empty($username) && !empty($password) && empty($email)){
-        
-        $hashed_password = md5($password);
-        
-        $json_array = $userObject->loginUsers($username, $hashed_password);
-        
-        echo json_encode($json_array);
-    }
-    ?>
+// Create connection
+$con=mysqli_connect("us-cdbr-iron-east-05.cleardb.net","bfee5eec5d1d2a","49334008","heroku_030aad63729d0bd");
+ 
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+ 
+// Select all of our stocks from table 'users'
+$sql = "SELECT * FROM users";
+ 
+// Confirm there are results
+if ($result = mysqli_query($con, $sql))
+{
+ // We have results, create an array to hold the results
+        // and an array to hold the data
+ $resultArray = array();
+ $tempArray = array();
+ 
+ // Loop through each result
+ while($row = $result->fetch_object())
+ {
+ // Add each result into the results array
+ $tempArray = $row;
+     array_push($resultArray, $tempArray);
+ }
+ 
+ // Encode the array to JSON and output the results
+ echo json_encode($resultArray);
+}
+ 
+// Close connections
+mysqli_close($con);
+?>
