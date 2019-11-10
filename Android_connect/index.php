a3856b3215918ca1e54c8a6a143bdc894e37f5d6
Keith Mahony <36074197+KeithMahony@users.noreply.github.com>
2019-11-10 12:09:10 +0000

    Create index.php

---
+++ Android_connect/index.php
@@ -0,0 +1,41 @@
+<?php
+    
+    require_once 'user.php';
+    
+    $username = "";
+    
+    $password = "";
+    
+    $email = "";
+    
+    if(isset($_POST['username'])){
+        
+        $username = $_POST['username'];
+        
+    }
+    
+    if(isset($_POST['password'])){
+        
+        $password = $_POST['password'];
+        
+    }
+    
+    if(isset($_POST['email'])){
+        
+        $email = $_POST['email'];
+        
+    }
+    
+    $userObject = new User();
+    
+    // Login
+    
+    if(!empty($username) && !empty($password) && empty($email)){
+        
+        $hashed_password = md5($password);
+        
+        $json_array = $userObject->loginUsers($username, $hashed_password);
+        
+        echo json_encode($json_array);
+    }
+    ?>
