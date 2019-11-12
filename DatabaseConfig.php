fcdb934a8961a0f77f5ecdd6ab01cc59df836d3d
Matthew Byrne <32844656+X17138744@users.noreply.github.com>
2019-11-12 15:26:22 +0000

    Update DatabaseConfig.php

--- DatabaseConfig.php
+++ DatabaseConfig.php
@@ -1,1 +1,15 @@
+<?php
 
+//Define your host here.
+$HostName = "us-cdbr-iron-east-05.cleardb.net";
+
+//Define your database username here.
+$HostUser = "bfee5eec5d1d2a";
+
+//Define your database password here.
+$HostPass = "49334008";
+
+//Define your database name here.
+$DatabaseName = "heroku_030aad63729d0bd";
+
+?>
