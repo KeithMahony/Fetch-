025f3c98bd7105e0e99491266ccfbb6c51a08829
Matthew Byrne <32844656+X17138744@users.noreply.github.com>
2019-11-12 15:39:05 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
  <?php
 if($_SERVER['REQUEST_METHOD']=='POST'){
-
- include 'DatabaseConfig.php';
  
- $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+ $con = mysqli_connect("us-cdbr-iron-east-05.cleardb.net","bfee5eec5d1d2a","49334008","heroku_030aad63729d0bd");
 
  $User_Email = $_POST['email'];
  
